7aaa503976a50ae8274f663ee8deee93da03667d
Roshan Kumar <116620615+roshankrr@users.noreply.github.com>
2025-10-14 09:00:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int size=nums.size();
+        int s=0,y=1;
+        for(int i=0;i<size-1;i++){
+            if(nums[s]==0 and nums[y]==0){
+                y++;
+                }
+            else if(nums[s]==0 and nums[y]!=0){
+            swap(nums[s],nums[y]);
+            s++;
+            y++;
+            }
+            // for(auto it:nums){
+            //     cout<<it<<" ";
+            // }
+            // cout<<endl;
+        }
+        // 1 0 0 3 12
+
+    }
+};
